874be361b781a00cf6ffc78b0a6a5fd6bbe55aaa
bylts <bylts.3@gmail.com>
2022-04-18 21:58:04 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const fs = require('fs')
+const path = require('path')
+const winkNLP = require('wink-nlp')
+const sentiment = require('wink-sentiment')
+const model = require('wink-eng-lite-model')
+const utils = require('wink-nlp-utils')
+const nlp = winkNLP(model)
+
+
+const inputPath = path.join(__dirname, 'input')
+const outputPath = path.join(__dirname, 'output')
+
+console.log('Running...')
+
+fs.readdir(inputPath, function (err, files) {
+    if (err) {
+        return console.log('Unable to scan directory: ' + err)
+    } 
+    files.forEach(function (file) {
+        fs.readFile(path.join(inputPath, file), 'utf8', (err, data) => {
+            if(err) {
+                console.log(err)
+                return
+            }
+            console.log(`READING ${file}`)
+            parseData(file, data)
+        })
+    })
+})
+
+function parseData(file, data) {
+    console.log(`PARSING ${file}`)
+    const doc = nlp.readDoc(data);
+    let content = doc.out()
+    let sentences = doc.sentences().out()
+
+    let line = 1
+    let csv = `"sentence number","sentiment","text"`
+    sentences.forEach(sentence => {
+        line++
+        let score = sentiment(sentence).score
+        let words = `${sentence}`
+        let csvline = `\n"${line}","${score}","${words}"`
+
+        csv += csvline
+    })
+
+    fs.writeFileSync(path.join(outputPath, `${file}.csv`), csv, {
+        encoding: 'utf8',
+        flag: 'w'
+    })
+}
+
+
+
